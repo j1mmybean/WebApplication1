29f10abab24262c94eb4d817140b99e3c6ee6211
j1mmybean <carb4305@gmail.com>
2023-03-17 16:35:09 +0800

    加入專案檔案。

---
+++ Controllers/JqController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class JqController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, HttpPost]
+        public IActionResult Login(string username, string password)
+        {           
+            if (username == "hello" && password == "kitty")
+                ViewData["LoginStatus"] = "pass";
+            else
+                ViewData["LoginStatus"] = "fail";
+
+            return View();
+        }
+    }
+}
